4ca5fc63d6021fd82818a2bfd5150b81ff7aa677
wi gig <wigig@mea.qualcomm.com>
2017-07-06 21:34:48 +0300

    Solved angular mistake
    just humen errors

--- app.js
+++ app.js
@@ -1,27 +1,17 @@
-var myApp = angular.module('myApp', ['ngRoute']);
-
-myApp.config(['$routeProvider','$locationProvider', function($routeProvider, $locationProvider) {
-	
-	$routeProvider
-		.when('/reports', {
-			templateUrl: 'pages/reports/index.html',
-			controller: 'reportCtrl'
-	})
-//		.otherwise({redirectTo: '/'});
-	
-	$locationProvider.hashPrefix('');
-//		.html5Mode({
-//			enabled: true,
-//			requireBase: false
-//	});
-}]);
-
-myApp.controller('reportCtrl', ['$scope', '$location','$http', '$log', function ($scope, $location, $http, $log) {
-	$http.get('http://localhost:3000/reports')
-	.then(function(result) {
-//		console.log(result.data);
-		$scope.reports=result.data;
-	});
-	$log.info($location.path());
-//	$log.info('test');
-}]);
+//var myApp = angular.module('myApp', []);
+//
+////myApp.config(['$routeProvider','$locationProvider', function($routeProvider, $locationProvider) {
+////	
+////	$routeProvider
+////		.when('/reports', {
+////			templateUrl: 'pages/reports/index.html',
+////			controller: 'reportCtrl'
+////	})
+////		.otherwise({redirectTo: '/reports'});
+////	
+////	$locationProvider.html5Mode(true).hashPrefix('');
+////}]);
+////	myApp.controller('registerCtrl', ['$scope','$http','$location', function($scope, $http, $location) {
+////		$scope.try = "test!!!";
+////		console.log("log");	
+////}]);
