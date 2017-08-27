44f4b5d5f6fca8f59e3661e42b2382106c0ad84f
Matan <matan.wittner@gmail.com>
2017-08-28 02:19:18 +0300

    Have to configure plans
    according to test params

--- application/logs/log-2017-08-28.php
+++ application/logs/log-2017-08-28.php
@@ -17,3 +17,21 @@ ERROR - 201720172017-0808-2828 0101:0808:4242 --> Severity: Notice --> Trying to
 ERROR - 201720172017-0808-2828 0101:0808:4242 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\application\controllers\auth.php 24
 ERROR - 201720172017-0808-2828 0101:0808:2424 --> Severity: Notice --> Undefined variable: channel C:\xampp\htdocs\application\models\plan_model.php 42
 ERROR - 201720172017-0808-2828 0101:0808:2424 --> Severity: Notice --> Undefined variable: antenna C:\xampp\htdocs\application\models\plan_model.php 48
+ERROR - 201720172017-0808-2828 0101:0808:1717 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\application\controllers\plans.php 28
+ERROR - 201720172017-0808-2828 0101:0808:1717 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\application\controllers\plans.php 30
+ERROR - 201720172017-0808-2828 0101:0808:1717 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\application\controllers\plans.php 31
+ERROR - 201720172017-0808-2828 0101:0808:1717 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\application\controllers\plans.php 33
+ERROR - 201720172017-0808-2828 0101:0808:1717 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\application\controllers\plans.php 34
+ERROR - 201720172017-0808-2828 0101:0808:1717 --> Severity: Notice --> Undefined property: stdClass::$antenna C:\xampp\htdocs\application\controllers\plans.php 42
+ERROR - 201720172017-0808-2828 0101:0808:1717 --> Severity: Notice --> Undefined property: stdClass::$ants C:\xampp\htdocs\application\controllers\plans.php 43
+ERROR - 201720172017-0808-2828 0101:0808:1717 --> Severity: Notice --> Undefined property: stdClass::$lineups C:\xampp\htdocs\application\controllers\plans.php 43
+ERROR - 201720172017-0808-2828 0101:0808:1717 --> Severity: Notice --> Undefined property: stdClass::$seconds C:\xampp\htdocs\application\controllers\plans.php 43
+ERROR - 201720172017-0808-2828 0101:0808:1717 --> Severity: Notice --> Undefined property: stdClass::$pins C:\xampp\htdocs\application\controllers\plans.php 43
+ERROR - 201720172017-0808-2828 0101:0808:1717 --> Severity: Notice --> Undefined property: stdClass::$priority C:\xampp\htdocs\application\controllers\plans.php 45
+ERROR - 201720172017-0808-2828 0101:0808:1717 --> Severity: Notice --> Undefined property: stdClass::$pinFrom C:\xampp\htdocs\application\controllers\plans.php 49
+ERROR - 201720172017-0808-2828 0101:0808:1717 --> Severity: Notice --> Undefined property: stdClass::$pinTo C:\xampp\htdocs\application\controllers\plans.php 50
+ERROR - 201720172017-0808-2828 0101:0808:1717 --> Severity: Notice --> Undefined property: stdClass::$pinStep C:\xampp\htdocs\application\controllers\plans.php 51
+ERROR - 201720172017-0808-2828 0101:0808:1717 --> Severity: Notice --> Undefined property: stdClass::$pinAdd C:\xampp\htdocs\application\controllers\plans.php 52
+ERROR - 201720172017-0808-2828 0101:0808:1717 --> Severity: Notice --> Undefined property: stdClass::$mcs C:\xampp\htdocs\application\controllers\plans.php 53
+ERROR - 201720172017-0808-2828 0101:0808:1717 --> Severity: 4096 --> Object of class stdClass could not be converted to string C:\xampp\htdocs\system\database\DB_driver.php 1476
+ERROR - 201720172017-0808-2828 0101:0808:1717 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ' NULL, NULL, NULL, NULL, NULL, 123, '123456', 0, 14)' at line 1 - Invalid query: INSERT INTO `tests` (`priority`, `lineup`, `station`, `name`, `pin_from`, `pin_to`, `pin_step`, `pin_additional`, `mcs`, `voltage`, `notes`, `seconds`, `plan_id`) VALUES (NULL, '123456', 'M-CB1', , NULL, NULL, NULL, NULL, NULL, 123, '123456', 0, 14)
