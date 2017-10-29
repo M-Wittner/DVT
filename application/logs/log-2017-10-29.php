a53901c2538652d903f5df574ddd1ebf8b3f9dc9
wi gig <wigig@mea.qualcomm.com>
2017-10-30 01:08:55 +0200

    nga css added

--- application/logs/log-2017-10-29.php
+++ application/logs/log-2017-10-29.php
@@ -16,3 +16,13 @@ ERROR - 201720172017-1010-2929 1313:1010:3333 --> Severity: Notice --> Trying to
 ERROR - 201720172017-1010-2929 1313:1010:3333 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\application\models\plan_model.php 451
 ERROR - 201720172017-1010-2929 1313:1010:3333 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\application\models\plan_model.php 451
 ERROR - 201720172017-1010-2929 1313:1010:3333 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\application\models\plan_model.php 451
+ERROR - 201720172017-1010-2929 1616:1010:2424 --> Severity: Notice --> Undefined property: stdClass::$password C:\xampp\htdocs\application\controllers\auth.php 24
+ERROR - 201720172017-1010-2929 1616:1010:4444 --> Severity: Notice --> Undefined variable: channel C:\xampp\htdocs\application\models\plan_model.php 49
+ERROR - 201720172017-1010-2929 1717:1010:1818 --> Severity: 4096 --> Object of class stdClass could not be converted to string C:\xampp\htdocs\system\database\DB_query_builder.php 1754
+ERROR - 201720172017-1010-2929 1717:1010:1818 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ')' at line 1 - Invalid query: REPLACE INTO `test_xifs` (`test_id`, `plan_id`, `chip_id`, `chip`, `xif`) VALUES ('316', '286', '145', 'YB37700150_TT', )
+ERROR - 201720172017-1010-2929 1717:1010:2424 --> Severity: 4096 --> Object of class stdClass could not be converted to string C:\xampp\htdocs\system\database\DB_query_builder.php 1754
+ERROR - 201720172017-1010-2929 1717:1010:2424 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ')' at line 1 - Invalid query: REPLACE INTO `test_xifs` (`test_id`, `plan_id`, `chip_id`, `chip`, `xif`) VALUES ('316', '286', '145', 'YB37700150_TT', )
+ERROR - 201720172017-1010-2929 1717:1010:2020 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1010-2929 1717:1010:2020 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1010-2929 1818:1010:1919 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1010-2929 1818:1010:0606 --> 404 Page Not Found: Faviconico/index
