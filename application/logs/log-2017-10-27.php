c693d7b0db5d29e210e7d17e941ffc72aa5028d5
wi gig <wigig@mea.qualcomm.com>
2017-10-29 15:08:23 +0200

    log

---
+++ application/logs/log-2017-10-27.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 201720172017-1010-2727 1010:1010:5555 --> Severity: Notice --> Undefined variable: channel C:\xampp\htdocs\application\models\plan_model.php 49
---
+++ application/logs/log-2017-10-29.php
@@ -0,0 +1,18 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 201720172017-1010-2929 1010:1010:3535 --> Severity: Notice --> Undefined variable: channel C:\xampp\htdocs\application\models\plan_model.php 49
+ERROR - 201720172017-1010-2929 1010:1010:4040 --> Severity: Notice --> Undefined variable: channel C:\xampp\htdocs\application\models\plan_model.php 49
+ERROR - 201720172017-1010-2929 1313:1010:5959 --> Severity: Notice --> Undefined variable: channel C:\xampp\htdocs\application\models\plan_model.php 49
+ERROR - 201720172017-1010-2929 1313:1010:1212 --> Severity: Notice --> Undefined variable: channel C:\xampp\htdocs\application\models\plan_model.php 49
+ERROR - 201720172017-1010-2929 1313:1010:3333 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\application\models\plan_model.php 447
+ERROR - 201720172017-1010-2929 1313:1010:3333 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\application\models\plan_model.php 447
+ERROR - 201720172017-1010-2929 1313:1010:3333 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\application\models\plan_model.php 448
+ERROR - 201720172017-1010-2929 1313:1010:3333 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\application\models\plan_model.php 448
+ERROR - 201720172017-1010-2929 1313:1010:3333 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\application\models\plan_model.php 449
+ERROR - 201720172017-1010-2929 1313:1010:3333 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\application\models\plan_model.php 449
+ERROR - 201720172017-1010-2929 1313:1010:3333 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\application\models\plan_model.php 451
+ERROR - 201720172017-1010-2929 1313:1010:3333 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\application\models\plan_model.php 451
+ERROR - 201720172017-1010-2929 1313:1010:3333 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\application\models\plan_model.php 451
+ERROR - 201720172017-1010-2929 1313:1010:3333 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\application\models\plan_model.php 451
+ERROR - 201720172017-1010-2929 1313:1010:3333 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\application\models\plan_model.php 451
+ERROR - 201720172017-1010-2929 1313:1010:3333 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\application\models\plan_model.php 451
