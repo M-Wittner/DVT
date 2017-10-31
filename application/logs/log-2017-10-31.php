9856583ed901cdcf8000076f67fcb85ae35fe9ad
wi gig <wigig@mea.qualcomm.com>
2017-10-31 18:17:55 +0200

    logs update

---
+++ application/logs/log-2017-10-31.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 201720172017-1010-3131 1616:1010:3434 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1010-3131 1616:1010:3434 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1010-3131 1616:1010:1717 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1010-3131 1616:1010:4646 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1010-3131 1616:1010:1313 --> 404 Page Not Found: Faviconico/index
