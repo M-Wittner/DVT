699036dac82b3923e6a7dcd470504352ca96646a
wi gig <wigig@mea.qualcomm.com>
2017-10-31 19:28:55 +0200

    logs

--- application/logs/log-2017-10-31.php
+++ application/logs/log-2017-10-31.php
@@ -5,3 +5,4 @@ ERROR - 201720172017-1010-3131 1616:1010:3434 --> 404 Page Not Found: Faviconico
 ERROR - 201720172017-1010-3131 1616:1010:1717 --> 404 Page Not Found: Faviconico/index
 ERROR - 201720172017-1010-3131 1616:1010:4646 --> 404 Page Not Found: Faviconico/index
 ERROR - 201720172017-1010-3131 1616:1010:1313 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1010-3131 1818:1010:2727 --> 404 Page Not Found: Faviconico/index
