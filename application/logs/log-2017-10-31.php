15b0ccbeb028ede6ade943a747ec78137469b903
wi gig <wigig@mea.qualcomm.com>
2017-10-31 17:51:11 +0200

    panel-default animatoipn

---
+++ application/logs/log-2017-10-31.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 201720172017-1010-3131 0303:1010:1212 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1010-3131 0606:1010:4343 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1010-3131 0707:1010:2929 --> 404 Page Not Found: Faviconico/index
