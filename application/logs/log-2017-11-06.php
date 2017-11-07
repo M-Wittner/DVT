2f3bcf022fbad81ccb9b8765c29e9795d65fe0d4
wi gig <wigig@mea.qualcomm.com>
2017-11-07 09:19:21 +0200

    logs

--- application/logs/log-2017-11-06.php
+++ application/logs/log-2017-11-06.php
@@ -4,3 +4,34 @@ ERROR - 201720172017-1111-0606 0000:1111:3838 --> Severity: Notice --> Undefined
 ERROR - 201720172017-1111-0606 0000:1111:3838 --> Severity: Notice --> Undefined variable: channel C:\xampp\htdocs\application\models\plan_model.php 49
 ERROR - 201720172017-1111-0606 0000:1111:3434 --> 404 Page Not Found: Faviconico/index
 ERROR - 201720172017-1111-0606 0000:1111:3434 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-0606 0303:1111:2424 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-0606 0303:1111:2424 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-0606 0404:1111:2121 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-0606 0404:1111:4444 --> Severity: Notice --> Undefined variable: channel C:\xampp\htdocs\application\models\plan_model.php 49
+ERROR - 201720172017-1111-0606 0404:1111:4444 --> Severity: Notice --> Undefined variable: channel C:\xampp\htdocs\application\models\plan_model.php 49
+ERROR - 201720172017-1111-0606 0404:1111:4343 --> Severity: Notice --> Undefined variable: channel C:\xampp\htdocs\application\models\plan_model.php 49
+ERROR - 201720172017-1111-0606 0404:1111:4343 --> Severity: Notice --> Undefined variable: channel C:\xampp\htdocs\application\models\plan_model.php 49
+ERROR - 201720172017-1111-0606 0707:1111:3838 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-0606 0707:1111:0909 --> Severity: Notice --> Undefined variable: channel C:\xampp\htdocs\application\models\plan_model.php 49
+ERROR - 201720172017-1111-0606 0707:1111:0909 --> Severity: Notice --> Undefined variable: channel C:\xampp\htdocs\application\models\plan_model.php 49
+ERROR - 201720172017-1111-0606 0808:1111:5050 --> Severity: Notice --> Undefined variable: channel C:\xampp\htdocs\application\models\plan_model.php 49
+ERROR - 201720172017-1111-0606 0808:1111:5050 --> Severity: Notice --> Undefined variable: channel C:\xampp\htdocs\application\models\plan_model.php 49
+ERROR - 201720172017-1111-0606 1616:1111:2222 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-0606 1616:1111:5656 --> Severity: Notice --> Undefined variable: channel C:\xampp\htdocs\application\models\plan_model.php 49
+ERROR - 201720172017-1111-0606 1616:1111:5656 --> Severity: Notice --> Undefined variable: channel C:\xampp\htdocs\application\models\plan_model.php 49
+ERROR - 201720172017-1111-0606 1717:1111:0404 --> Query error: Duplicate entry 'YA951020005_TT' for key 'serial_number' - Invalid query: INSERT INTO `params_chips` (`chip`, `board`, `package`, `model`, `revision`, `serial_number`, `corner`) VALUES ('TalynA', 'YA591-H1', 'Flip Chip', 'QCA6425', 'A0', 'YA951020005_TT', 'TT')
+ERROR - 201720172017-1111-0606 1717:1111:1010 --> Query error: Duplicate entry 'YA951020005_TT' for key 'serial_number' - Invalid query: INSERT INTO `params_chips` (`chip`, `board`, `package`, `model`, `revision`, `serial_number`, `corner`) VALUES ('TalynA', 'YA591-H1', 'Flip Chip', 'QCA6425', 'A0', 'YA951020005_TT', 'TT')
+ERROR - 201720172017-1111-0606 1717:1111:1515 --> Query error: Duplicate entry 'YA951020005_TT' for key 'serial_number' - Invalid query: INSERT INTO `params_chips` (`chip`, `board`, `package`, `model`, `revision`, `serial_number`, `corner`) VALUES ('TalynA', 'YA591-H1', 'Flip Chip', 'QCA6425', 'A0', 'YA951020005_TT', 'TT')
+ERROR - 201720172017-1111-0606 1717:1111:1616 --> Query error: Duplicate entry 'YA951020005_TT' for key 'serial_number' - Invalid query: INSERT INTO `params_chips` (`chip`, `board`, `package`, `model`, `revision`, `serial_number`, `corner`) VALUES ('TalynA', 'YA591-H1', 'Flip Chip', 'QCA6425', 'A0', 'YA951020005_TT', 'TT')
+ERROR - 201720172017-1111-0606 1717:1111:1717 --> Query error: Duplicate entry 'YA951020005_TT' for key 'serial_number' - Invalid query: INSERT INTO `params_chips` (`chip`, `board`, `package`, `model`, `revision`, `serial_number`, `corner`) VALUES ('TalynA', 'YA591-H1', 'Flip Chip', 'QCA6425', 'A0', 'YA951020005_TT', 'TT')
+ERROR - 201720172017-1111-0606 1717:1111:1717 --> Query error: Duplicate entry 'YA951020005_TT' for key 'serial_number' - Invalid query: INSERT INTO `params_chips` (`chip`, `board`, `package`, `model`, `revision`, `serial_number`, `corner`) VALUES ('TalynA', 'YA591-H1', 'Flip Chip', 'QCA6425', 'A0', 'YA951020005_TT', 'TT')
+ERROR - 201720172017-1111-0606 1717:1111:1717 --> Query error: Duplicate entry 'YA951020005_TT' for key 'serial_number' - Invalid query: INSERT INTO `params_chips` (`chip`, `board`, `package`, `model`, `revision`, `serial_number`, `corner`) VALUES ('TalynA', 'YA591-H1', 'Flip Chip', 'QCA6425', 'A0', 'YA951020005_TT', 'TT')
+ERROR - 201720172017-1111-0606 1717:1111:1717 --> Query error: Duplicate entry 'YA951020005_TT' for key 'serial_number' - Invalid query: INSERT INTO `params_chips` (`chip`, `board`, `package`, `model`, `revision`, `serial_number`, `corner`) VALUES ('TalynA', 'YA591-H1', 'Flip Chip', 'QCA6425', 'A0', 'YA951020005_TT', 'TT')
+ERROR - 201720172017-1111-0606 1717:1111:4949 --> Severity: Notice --> Undefined variable: channel C:\xampp\htdocs\application\models\plan_model.php 49
+ERROR - 201720172017-1111-0606 1717:1111:4949 --> Severity: Notice --> Undefined variable: channel C:\xampp\htdocs\application\models\plan_model.php 49
+ERROR - 201720172017-1111-0606 1818:1111:4545 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-0606 1818:1111:4545 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-0606 2020:1111:5252 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-0606 2020:1111:5252 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-0606 2323:1111:3636 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-0606 2323:1111:1010 --> 404 Page Not Found: Faviconico/index
---
+++ application/logs/log-2017-11-07.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 201720172017-1111-0707 0707:1111:0909 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-0707 0707:1111:0909 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-0707 0808:1111:2525 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-0707 0808:1111:2727 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-0707 0808:1111:2727 --> 404 Page Not Found: Faviconico/index
