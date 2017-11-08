d36563d73f50b1566f64533e5f3c8eba5d87295a
wi gig <wigig@mea.qualcomm.com>
2017-11-08 09:07:48 +0200

    logs

--- application/logs/log-2017-11-07.php
+++ application/logs/log-2017-11-07.php
@@ -6,3 +6,17 @@ ERROR - 201720172017-1111-0707 0808:1111:2525 --> 404 Page Not Found: Faviconico
 ERROR - 201720172017-1111-0707 0808:1111:2727 --> 404 Page Not Found: Faviconico/index
 ERROR - 201720172017-1111-0707 0808:1111:2727 --> 404 Page Not Found: Faviconico/index
 ERROR - 201720172017-1111-0707 1212:1111:3939 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-0707 1616:1111:4343 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-0707 1616:1111:1212 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-0707 1616:1111:1010 --> Severity: Notice --> Undefined property: stdClass::$voltage C:\xampp\htdocs\application\controllers\plans.php 70
+ERROR - 201720172017-1111-0707 1616:1111:1010 --> Query error: Column 'voltage' cannot be null - Invalid query: INSERT INTO `tests` (`priority`, `lineup`, `station`, `name`, `pin_from`, `pin_to`, `pin_step`, `pin_additional`, `mcs`, `voltage`, `notes`, `seconds`, `plan_id`) VALUES ('1', '\\\\filer4\\fileserver\\Projects\\dvt\\lineups\\TalynA\\R-CB2\\night run 071117', 'R-CB2', 'TX-EVM', -30, -10, 10, '-44', 12, NULL, NULL, NULL, 309)
+ERROR - 201720172017-1111-0707 1616:1111:1818 --> Severity: Notice --> Undefined property: stdClass::$voltage C:\xampp\htdocs\application\controllers\plans.php 70
+ERROR - 201720172017-1111-0707 1616:1111:1818 --> Query error: Column 'voltage' cannot be null - Invalid query: INSERT INTO `tests` (`priority`, `lineup`, `station`, `name`, `pin_from`, `pin_to`, `pin_step`, `pin_additional`, `mcs`, `voltage`, `notes`, `seconds`, `plan_id`) VALUES ('1', '\\\\filer4\\fileserver\\Projects\\dvt\\lineups\\TalynA\\R-CB2\\night run 071117', 'R-CB2', 'TX-EVM', -30, -10, 10, '-44', 12, NULL, NULL, NULL, 310)
+ERROR - 201720172017-1111-0707 1616:1111:0404 --> Severity: Notice --> Undefined property: stdClass::$voltage C:\xampp\htdocs\application\controllers\plans.php 70
+ERROR - 201720172017-1111-0707 1616:1111:0404 --> Query error: Column 'voltage' cannot be null - Invalid query: INSERT INTO `tests` (`priority`, `lineup`, `station`, `name`, `pin_from`, `pin_to`, `pin_step`, `pin_additional`, `mcs`, `voltage`, `notes`, `seconds`, `plan_id`) VALUES ('1', '\\\\filer4\\fileserver\\Projects\\dvt\\lineups\\TalynA\\R-CB2\\night run 071117', 'R-CB2', 'TX-EVM', -30, -10, 10, '-44', 12, NULL, NULL, NULL, 312)
+ERROR - 201720172017-1111-0707 1616:1111:2222 --> Severity: Notice --> Undefined property: stdClass::$voltage C:\xampp\htdocs\application\controllers\plans.php 70
+ERROR - 201720172017-1111-0707 1616:1111:2222 --> Query error: Column 'voltage' cannot be null - Invalid query: INSERT INTO `tests` (`priority`, `lineup`, `station`, `name`, `pin_from`, `pin_to`, `pin_step`, `pin_additional`, `mcs`, `voltage`, `notes`, `seconds`, `plan_id`) VALUES ('1', '\\\\filer4\\fileserver\\Projects\\dvt\\lineups\\TalynA\\R-CB2\\night run 071117', 'R-CB2', 'TX-EVM', -30, -10, 10, '-44', 12, NULL, NULL, NULL, 313)
+ERROR - 201720172017-1111-0707 1818:1111:4747 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-0707 1818:1111:4747 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-0707 2020:1111:3838 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-0707 2020:1111:3838 --> 404 Page Not Found: Faviconico/index
---
+++ application/logs/log-2017-11-08.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 201720172017-1111-0808 0707:1111:1616 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-0808 0707:1111:1616 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-0808 0707:1111:1010 --> Severity: Notice --> Undefined variable: channel C:\xampp\htdocs\application\models\plan_model.php 49
+ERROR - 201720172017-1111-0808 0707:1111:1010 --> Severity: Notice --> Undefined variable: channel C:\xampp\htdocs\application\models\plan_model.php 49
