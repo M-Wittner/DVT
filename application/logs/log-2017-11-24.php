632e181bd05b5e4f09eb6023dd709fe5233db1d5
wi gig <wigig@mea.qualcomm.com>
2017-11-26 16:56:27 +0200

    logs

---
+++ application/logs/log-2017-11-24.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 201720172017-1111-2424 0808:1111:3434 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-2424 0808:1111:1212 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-2424 1111:1111:5151 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-2424 1111:1111:5151 --> 404 Page Not Found: Faviconico/index
---
+++ application/logs/log-2017-11-26.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 201720172017-1111-2626 0707:1111:0000 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-2626 0808:1111:1515 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-2626 1515:1111:5656 --> 404 Page Not Found: Faviconico/index
+ERROR - 201720172017-1111-2626 1515:1111:5656 --> 404 Page Not Found: Faviconico/index
